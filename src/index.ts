ce3abc44b90043d1886929743010f531966bfcf9
CheryX <roxmb25@gmail.com>
2022-08-06 12:56:33 +0200

    Only convert .pol files

--- src/index.ts
+++ src/index.ts
@@ -8,14 +8,15 @@ const config = JSON.parse(fs.readFileSync("konfiguracja.json", 'utf8'));
 // Run a conversion algorithm for every file in input directory
 fs.readdir(config.folderWejścia, (err, files) => {
 
-    for (let i = 0; i <= files.length; i++) {
+    for (let i = 0; i < files.length; i++) {
 
-        const filePath = `${config.folderWejścia}/${files[i]}`
+        const filePath = `${config.folderWejścia}/${files[i]}`;
+        const outPath = `${config.folderWyjścia}/${files[i].replace('.pol', '.js')}`;
 
-        // TODO: Convert only .pol files
-        console.log (filePath.split('.').pop() )
+        const fileFormat = filePath.split('.').pop();
+        if (fileFormat != 'pol') continue;
 
-        const content = fs.readFileSync(filePath, 'utf8');
+        const content = fs.readFileSync(outPath, 'utf8');
         fs.writeFileSync(filePath.replace('.pol', '.js'), convertToJs(content));
 
     };
