15147131ab9f73cdd5c82ba69278d73b5188ebc9
CheryX <maciejmusielik@gmail.com>
2022-12-24 15:14:55 +0100

    Created Lexer file

--- models/Lexer.js
+++ models/Lexer.js
@@ -1,1 +1,49 @@
 //prędzej napisze fraszkę niż ten kod
+
+const operators = /+-/g
+
+class Lexer {
+
+    constructor(content) {
+
+        this.content = content;
+        this.charIndex = 0;
+        
+        lexize();
+
+    }
+
+    lexize() {
+
+    }
+
+}
+
+"2 + 2 * 4;"
+
+[
+    {
+        type: "INTEGER",
+        content: "2"
+    },
+    {
+        type: "OPERATOR",
+        content: "ADDITION"
+    },
+    {
+        type: "INTEGER",
+        content: "2"
+    },
+    {
+        type: "OPERATOR",
+        content: "MULTIPLICATION"
+    },
+    {
+        type: "INTEGER",
+        content: "4"
+    },
+    {
+        type: "END",
+        content: "DEFAULT"
+    }
+]
