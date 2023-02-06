a91ff31a58621fa30690274f06931b1304a7e282
CheryX <maciejmusielik@gmail.com>
2023-02-06 20:21:01 +0100

    Honestly I have no idea what's going on here anymore

--- models/Lexer.js
+++ models/Lexer.js
@@ -1,49 +1,52 @@
-//prędzej napisze fraszkę niż ten kod
-
-const operators = /+-/g
-
 class Lexer {
+    constructor(input) {
+        this.input = input;
+        this.pos = 0;
+    }
+  
+    nextToken() {
+        while (this.pos < this.input.length) {
+            let char = this.input[this.pos];
+            if (char === ' ') {
+                this.pos++;
+                continue;
+            }
+            if (char === '+') {
+                this.pos++;
+                return { type: 'PLUS', value: '+' };
+            }
+            if (char === '-') {
+                this.pos++;
+                return { type: 'MINUS', value: '-' };
+            }
+            if (/\d/.test(char)) {
+                let value = '';
+                while (this.pos < this.input.length && /\d/.test(this.input[this.pos])) {
+                    value += this.input[this.pos++];
+                }
+                return { type: 'INTEGER', value: parseInt(value) };
+            }
+            if (char === '"') {
+                let value = '';
+                this.pos++;
+                while (this.input[this.pos] !== '"') {
+                  value += this.input[this.pos++];
+                }
+                this.pos++;
+                return { type: 'STRING', value };
+            }
+            throw new Error(`Unexpected character: ${char}`);
+        }
 
-    constructor(content) {
-
-        this.content = content;
-        this.charIndex = 0;
-        
-        lexize();
+        return { type: 'EOF', value: null };
 
     }
-
-    lexize() {
-
-    }
-
 }
 
-"2 + 2 * 4;"
+let lexer = new Lexer("3 + 4 - 5 \"kurwa mać\"");
 
-[
-    {
-        type: "INTEGER",
-        content: "2"
-    },
-    {
-        type: "OPERATOR",
-        content: "ADDITION"
-    },
-    {
-        type: "INTEGER",
-        content: "2"
-    },
-    {
-        type: "OPERATOR",
-        content: "MULTIPLICATION"
-    },
-    {
-        type: "INTEGER",
-        content: "4"
-    },
-    {
-        type: "END",
-        content: "DEFAULT"
-    }
-]
+let currentToken = lexer.nextToken();
+while (currentToken.type !== 'EOF') {
+  console.log(currentToken);
+  currentToken = lexer.nextToken();
+}
