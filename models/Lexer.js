c4c8fe15da8fec0ec49897071a68ed90a2e313d5
CheryX <maciejmusielik@gmail.com>
2023-02-08 18:31:12 +0100

    Remove old KPL parser.

--- models/Lexer.js
+++ models/Lexer.js
@@ -118,11 +118,4 @@ class Lexer {
 
 }
   
-
-const lexer = new Lexer(`def function FUNCNAME args [A,B,C] callback 1 LINES1;`);
-
-let currentToken = lexer.getNextToken();
-while (currentToken.type !== "EOF") {
-  console.log(currentToken);
-  currentToken = lexer.getNextToken();
-}
+export default Lexer;
--- models/Parser.js
+++ models/Parser.js
@@ -1,79 +1,9 @@
-import translatedKeywords from "../utils/keywords.js";
-import getPoints from "./CQGrade.js";
-import r, { getSentences, matchOutsideQuotes, outsideConstructor } from "../utils/Regex.js";
+import Lexer from "./Lexer";
 
-/**
- * Converts .pol to .js
- * @param src Kochanowski.js file content
- * @param config Kochanowski configuration file
- * @returns JavaScript file content
- */
-export default function convertToJs(src, config) {
-    
-    let sentences = getSentences(src);
-    
-    // Making lives harder
-    if (!config || !config.bypassStyle) getPoints(src);
-    if (!config || !config.bypassErrors) throwRandomErrors(sentences);
+const lexer = new Lexer(`def function FUNCNAME args [A,B,C] callback 1 LINES1;`);
 
-    return wordConverter(sentences);
-
-}
-
-function wordConverter(lines) {
-
-    lines = lines.map(e => convertToLower(e));
-
-    for (let i in lines) {
-        for (let j in translatedKeywords) {
-            const keywords = new RegExp(`${translatedKeywords[j][0]}(?=([^"]*"[^"]*")*[^"]*$)`, 'g');
-
-            lines[i] = lines[i].replaceAll(keywords, translatedKeywords[j][1]);
-        }
-    }
-
-    return lines.join(';')+';'
-}
-
-/**
- * Checks if the code is written correctly
- * @param sentences Array of instructions
- */
-function throwRandomErrors(sentences) {
-
-    // Which symbols cannot be used outside quotations
-    const illegalSymbols = ['+', '-', '*', '/', '%']
-    const brackets = [["[", "]"], ["{", "}"], ["(", ")"]]
-
-    for (let i = 0; i < sentences.length; i++) {
-
-        if (sentences[i].match(r.firstCharLower))
-            throw new SyntaxError(`Błąd w linii ${i+1}: Rozpoczynanie zdań z małej litery (ort)`);
-
-        // Illegal characters
-        for (let j of illegalSymbols) {
-            if (matchOutsideQuotes(sentences[i], j))
-                throw new SyntaxError(`Błąd w linii ${i+1}: Używanie ang*elskich symboli (jęz) (Symbol ${j})`);
-        }
-
-        // Not matching brackets
-        for (let j of brackets) {
-            if (matchOutsideQuotes(sentences.join("\n"), j[0]) != matchOutsideQuotes(sentences.join("\n"), j[1]))
-                throw new SyntaxError(`Błąd w linii ${i+1}: Nie domykanie nawiasów (sens)`);
-        }
-    }
-}
-
-/**
- * Converts every character to lower case that isn't in quotes
- * @param content Content
- * @returns Modified string
- */
-export function convertToLower(content) {
-
-    // Drukuj("Witaj świecie"). => drukuj("Witaj Świecie").
-    return content.replaceAll(r.anythingOutsideQuotes, function(txt) {
-        return txt.toLocaleLowerCase()
-    });
-
+let currentToken = lexer.getNextToken();
+while (currentToken.type !== "EOF") {
+  console.log(currentToken);
+  currentToken = lexer.getNextToken();
 }
