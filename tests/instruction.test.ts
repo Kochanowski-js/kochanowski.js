775462af4a859566f10b19af5f5eaa9f5266b783
CheryX <maciejmusielik@gmail.com>
2024-07-23 16:37:18 +0200

    Fix typo in getRawInstruction

--- tests/instruction.test.ts
+++ tests/instruction.test.ts
@@ -1,20 +1,20 @@
-import { getRawInstrction } from "../src/instruction";
+import { getRawInstruction } from "../src/instruction";
 
 describe('Looking for patterns', () => {
   test('Recognizing patterns', () => {
-    expect(getRawInstrction('Zdefiniuj zmienną o nazwie Hatsune Miku o wartości równej くれわ').name).toBe("create");
-    expect(getRawInstrction('Zdefiniuj zmienną o wartości równej くれわ o nazwie Hatsune Miku').name).toBe("create_alt");
-    expect(getRawInstrction('Zmiennej o nazwie Hatsune Miku przypisz wartość równą くれわ').name).toBe("assign");
-    expect(getRawInstrction('Zmiennej Hatsune Miku przypisz wartość くれわ').name).toBe("assign_short");
-    expect(getRawInstrction('Przypisz wartość くれわ zmiennej o nazwie Hatsune Miku').name).toBe("assign_alt");
-    expect(getRawInstrction('Przypisz wartość くれわ zmiennej Hatsune Miku').name).toBe("assign_short_alt");
+    expect(getRawInstruction('Zdefiniuj zmienną o nazwie Hatsune Miku o wartości równej くれわ').name).toBe("create");
+    expect(getRawInstruction('Zdefiniuj zmienną o wartości równej くれわ o nazwie Hatsune Miku').name).toBe("create_reverse");
+    expect(getRawInstruction('Zmiennej o nazwie Hatsune Miku przypisz wartość równą くれわ').name).toBe("assign");
+    expect(getRawInstruction('Zmiennej Hatsune Miku przypisz wartość くれわ').name).toBe("assign_short");
+    expect(getRawInstruction('Przypisz wartość くれわ zmiennej o nazwie Hatsune Miku').name).toBe("assign_reverse");
+    expect(getRawInstruction('Przypisz wartość くれわ zmiennej Hatsune Miku').name).toBe("assign_reverse_no_name");
   });
 
   test('Recognizing invalid patterns', () => {
-    expect(getRawInstrction('Nie wiem').name).toBe("not_found");
+    expect(getRawInstruction('Nie wiem').name).toBe("not_found");
   });
 
   test('Evaluating variables', () => {
-    expect(getRawInstrction('Zdefiniuj zmienną o nazwie Hatsune Miku o wartości równej くれわ').values).toStrictEqual(['Hatsune Miku', 'くれわ']);
+    expect(getRawInstruction('Zdefiniuj zmienną o nazwie Hatsune Miku o wartości równej くれわ').values).toStrictEqual(['Hatsune Miku', 'くれわ']);
   })
 });
